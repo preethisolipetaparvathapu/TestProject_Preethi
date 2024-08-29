4c613fe11be4426ff559fdafeb75bc6d36c3ba65
Preethi_solipeta <preethisolipeta@gmail.com>
2024-08-28 17:52:38 -0700

    this is a stash commit

---
+++ src/StashClass.java
@@ -0,0 +1,12 @@
+
+public class StashClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this class is for stashing "); //to stash tracked and untracked files then say stash include all
+		//tracked files: is a file t at exiosts in local repository 
+		//untracked files : is a file newly created and never commited to the local repository 
+	}
+
+}
